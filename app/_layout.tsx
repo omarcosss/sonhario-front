8033a47f994f2033cc69e1f0acadae651e5ecb08
Marcos Costa <omarcosss@outlook.com>
2025-05-27 09:15:54 -0300

    💄 novos ícones

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar style='light' />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
